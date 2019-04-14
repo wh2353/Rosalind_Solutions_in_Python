9a88ed0928d3b29c123ab5a0ff91b662fdc11941
Wenrui Huang <wh2353@columbia.edu>
2019-04-14 02:39:51 -0400

    Update FULL.py

--- Bioinformatics Stronghold/FULL.py
+++ Bioinformatics Stronghold/FULL.py
@@ -5,7 +5,7 @@ fragment_weights = [round(eval(x),5) for x in data_file[1:]]
 
 
 #get the protein mass table
-protein_mass_table = open("/Users/WENRUI/Downloads/protein_mass_table.txt","r").read().rstrip().split("\n")
+protein_mass_table = open("protein_mass_table.txt","r").read().rstrip().split("\n")
 protein_mass_table = dict((x.split()[0], eval(x.split()[1])) for x in protein_mass_table)
 
 
