522413aeb934cfc4861f3bed8029787d02f6e117
Wenrui <wh2353@columbia.edu>
2019-04-26 18:28:51 -0400

    add solution for 'The Founder Effect and Genetic Drift'

--- Bioinformatics Stronghold/FOUN/FOUN.py
+++ Bioinformatics Stronghold/FOUN/FOUN.py
@@ -33,5 +33,5 @@ calculat the final distribution as dist_vec%*%power(transition_marix, g)
 output log10 probability of each factor with zero recessive allele
 '''
 for i in range(1, g+1):
-    final_dist_vec = (np.dot(matrix_power(transition_matrix, i), k_matrix.T))
+    final_dist_vec = np.dot(matrix_power(transition_matrix, i), k_matrix.T)
     print ' '.join(str(x) for x in np.log10(final_dist_vec[0, :len(k_vec)]))
