6f460eef578cef8e61a4b34ae089c4a507e7df7f
Wenrui <wh2353@columbia.edu>
2019-04-30 01:21:02 -0400

    add solution for building TRIE data structure for both problems TRIE and BA9A

---
+++ Bioinformatics Stronghold/TRIE/TRIE.py
@@ -0,0 +1,40 @@
+class Trie:
+    def __init__(self):
+        self.counter = count(start=1)
+        self.root = [next(self.counter), {}]
+    
+    def insert(self, sequence):
+        node = self.root
+        for base in sequence:
+            if base not in node[1]:
+                node[1][base] = [next(self.counter), {}] 
+            node = node[1][base]
+            
+def trie(list_of_seqs):
+    theTrie = Trie()
+    for seq in list_of_seqs:
+        theTrie.insert(seq)
+    return theTrie.root
+
+
+def Format(node):
+    #print result
+    for char, node2 in node[1].iteritems():
+        result.append(str(node[0]) + ' ' + str(node2[0]) + ' ' + str(char))
+        Format(node2)
+    return result
+
+
+
+if __name__ == '__main__':
+
+    from itertools import count
+    
+    
+    seqs=open("rosalind_trie.txt","r").read().rstrip().split("\n")
+    
+    result = []
+    f = open('output.txt','w')
+    for item in Format(trie(seqs)):
+        f.write(item + '\n')
+    f.close()
